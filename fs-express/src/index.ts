1078e9a5bdb49204639d9eab655d19e58297bcde
空海 <574980606@qq.com>
2022-03-30 17:14:06 +0800

    fix

--- fs-express/src/index.ts
+++ fs-express/src/index.ts
@@ -1,7 +1,9 @@
+import fs from 'fs';
 import cors from 'cors';
 import express, { json } from 'express';
 import { AddressInfo } from 'net';
-import { createRouter, readRouter } from './router';
+import { deserializer, join, serializer } from './utils';
+import { ErrorModel, SuccessModel } from './model';
 
 const app = express();
 
@@ -9,23 +11,23 @@ app.use(cors());
 app.use(json());
 
 app.get('/read', (req, res) => {
-  readRouter(req)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+  const { path } = req.query;
+  if (!path) {
+    res.status(500).send(new ErrorModel(null, '读取失败，请传入正确的path'));
+    return;
+  }
+  const buffer = fs.readFileSync(join(path as string));
+  res.send(new SuccessModel(deserializer(buffer.toString()), '读取成功'));
 });
 
 app.post('/create', (req, res) => {
-  createRouter(req)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+  const { path, content } = req.body;
+  if (!path || !content) {
+    res.status(500).send(new ErrorModel(null, '写入失败，请传入正确的参数'));
+    return;
+  }
+  fs.writeFileSync(join(path), serializer(content));
+  res.send(new SuccessModel(null, '写入成功'));
 });
 
 const server = app.listen(8080, () => {
