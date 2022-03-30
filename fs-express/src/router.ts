dfe7706ce78862efd247df4c5b25da53a016c0b9
空海 <574980606@qq.com>
2022-03-30 16:08:30 +0800

    fix

--- fs-express/src/router.ts
+++ fs-express/src/router.ts
@@ -1,5 +1,5 @@
+import fs from 'fs';
 import { Request } from 'express';
-import { readFile, writeFile } from 'fs-extra';
 import { ErrorModel, SuccessModel } from './model';
 import { join, serializer, deserializer } from './utils';
 
@@ -9,13 +9,12 @@ export const readRouter = (req: Request): Promise<SuccessModel> => {
     if (!path) {
       reject(new ErrorModel(null, '读取失败，请传入正确的path'));
     }
-    readFile(join(path as string))
-      .then(data => {
-        resolve(new SuccessModel(deserializer(data.toString()), '读取成功'));
-      })
-      .catch(err => {
+    fs.readFile(join(path as string), (err, data) => {
+      if (err) {
         reject(new ErrorModel(null, '读取失败：' + err));
-      });
+      }
+      resolve(new SuccessModel(deserializer(data.toString()), '读取成功'));
+    });
   });
 };
 
@@ -25,12 +24,11 @@ export const createRouter = (req: Request): Promise<SuccessModel> => {
     if (!path || !content) {
       reject(new ErrorModel(null, '写入失败，请传入正确的参数'));
     }
-    writeFile(join(path), serializer(content))
-      .then(() => {
-        resolve(new SuccessModel(null, '写入成功'));
-      })
-      .catch(err => {
+    fs.writeFile(join(path), serializer(content), err => {
+      if (err) {
         reject(new ErrorModel(null, '写入失败：' + err));
-      });
+      }
+      resolve(new SuccessModel(null, '写入成功'));
+    });
   });
 };
